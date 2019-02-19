482ba2628696d79412de3b5bce5c488934f901d2
Jeremy Trane <jeremy.trane@gmail.com>
2019-02-19 22:43:10 +0000

    Add french localisations

---
+++ ScET/Resources/Localisation/scet_fr.ts
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>MainFrame</name>
+    <message>
+        <location filename="Widgets/MainFrame.ui" line="17"/>
+        <location filename="Widgets/MainFrame.ui" line="71"/>
+        <source>ScET</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Widgets/MainFrame.ui" line="207"/>
+        <source>Start</source>
+        <translation>Lancer</translation>
+    </message>
+    <message>
+        <location filename="Widgets/MainFrame.ui" line="245"/>
+        <source>Stop</source>
+        <translation>Arrêter</translation>
+    </message>
+    <message>
+        <location filename="Widgets/MainFrame.ui" line="309"/>
+        <source>Save</source>
+        <translation>Sauvgarder</translation>
+    </message>
+    <message>
+        <location filename="Widgets/MainFrame.ui" line="344"/>
+        <source>Export</source>
+        <translation>Exporter</translation>
+    </message>
+    <message>
+        <location filename="Widgets/MainFrame.ui" line="379"/>
+        <source>Import</source>
+        <translation>Importer</translation>
+    </message>
+    <message>
+        <location filename="Widgets/MainFrame.cpp" line="29"/>
+        <source>Application Layer</source>
+        <translation>Application Layer</translation>
+    </message>
+    <message>
+        <location filename="Widgets/MainFrame.cpp" line="30"/>
+        <source>Protocol Layer</source>
+        <translation>Protocol Layer</translation>
+    </message>
+</context>
+<context>
+    <name>TitleBar</name>
+    <message>
+        <location filename="Widgets/TitleBar.cpp" line="30"/>
+        <source>Restore</source>
+        <translation>Niveau inf.</translation>
+    </message>
+    <message>
+        <location filename="Widgets/TitleBar.cpp" line="36"/>
+        <source>Minimize</source>
+        <translation>Réduire</translation>
+    </message>
+    <message>
+        <location filename="Widgets/TitleBar.cpp" line="41"/>
+        <source>Maximize</source>
+        <translation>Agrandir</translation>
+    </message>
+    <message>
+        <location filename="Widgets/TitleBar.cpp" line="48"/>
+        <source>Close</source>
+        <translation>Fermer</translation>
+    </message>
+</context>
+<context>
+    <name>TitleBarButton</name>
+    <message>
+        <location filename="Widgets/Buttons/TitleBarButton.cpp" line="26"/>
+        <source>Close</source>
+        <translation>Fermer</translation>
+    </message>
+    <message>
+        <location filename="Widgets/Buttons/TitleBarButton.cpp" line="30"/>
+        <source>Maximize</source>
+        <translation>Agrandir</translation>
+    </message>
+    <message>
+        <location filename="Widgets/Buttons/TitleBarButton.cpp" line="34"/>
+        <source>Restore</source>
+        <translation>Niveau inf.</translation>
+    </message>
+    <message>
+        <location filename="Widgets/Buttons/TitleBarButton.cpp" line="38"/>
+        <source>Minimize</source>
+        <translation>Réduire</translation>
+    </message>
+</context>
+</TS>
